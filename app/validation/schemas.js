a83a320d9819dd8c50d45f6d3b47b1e9c5e6a103
codekcg23 <kavigamage23@gmail.com>
2019-09-29 08:33:22 +0530

    20190929 -01 few changes done

--- app/validation/schemas.js
+++ app/validation/schemas.js
@@ -5,13 +5,12 @@ const schemas = {
 
     salesRep : Joi.object().keys({
 
-     //   id       : Joi.number().required(),
         userName : Joi.string().required(),
         fullName : Joi.string().required(),
         nic      : Joi.string().required(),
         area     : Joi.string().required(),
         address  : Joi.string().required(),
-        phoneNo  : Joi.number().required(),
+        phoneNo  : Joi.string().required(),
         email    : Joi.string().email().lowercase().optional(),
         password : Joi.string().required().strict(),
         confirmPassword : Joi.string().valid(Joi.ref('password')).required().strict()
