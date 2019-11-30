9d9281ad66a248baa97a64bbfc2208f9c532eaf3
Geesa-Vihara <geesavihara97@gmail.com>
2019-11-30 13:08:02 +0530

    20191130-01 Remove delete account

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -12,7 +12,7 @@ router.get("/getimage/:token",verifyToken, user.getimage);
 router.put('/update',verifyToken,user.update); 
 router.put('/updateusername',verifyToken,user.updateusername);  
 router.put('/updatepassword',verifyToken,user.updatepassword);  
-router.delete('/deleteaccount',verifyToken,user.deleteaccount); 
+//router.delete('/deleteaccount',verifyToken,user.deleteaccount); 
 router.post('/storeimage',verifyToken,user.storeimage)
 
 module.exports = router;
