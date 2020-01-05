bfc68fe468fd4b198e8a436e6746aed02dbb23b3
Geesa-Vihara <geesavihara97@gmail.com>
2020-01-05 18:39:27 +0530

    20200105-01 daily top performing outlet

--- app/controllers/dashboard.controller.js
+++ app/controllers/dashboard.controller.js
@@ -4,7 +4,7 @@ exports.dailyOrders=(req,res)=>{
     var n = new Date();
     n.setUTCHours(24,0,0,0); 
     /* var d=new Date("2019-12-09T00:00:00.000Z");
-    var n=new Date("2019-12-10T00:00:00.000Z"); */
+    var n=new Date("2019-12-10T00:00:00.000Z");  */
     Invoice
         .find({ orderDate: 
             {
@@ -14,6 +14,27 @@ exports.dailyOrders=(req,res)=>{
     })
         .then(invoice => {
             res.status(200).json(invoice);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        })
+}
+exports.topCustomer=(req,res)=>{
+    var d = new Date();
+    d.setUTCHours(0,0,0,0);
+    var n = new Date();
+    n.setUTCHours(24,0,0,0); 
+    /* var d=new Date("2019-12-09T00:00:00.000Z");
+    var n=new Date("2019-12-10T00:00:00.000Z");  */
+    Invoice
+        .find({ orderDate: 
+            {
+            $gte: new Date(d),
+            $lt: new Date(n)
+        }
+    }).sort({totalValue:-1}).limit(1)
+        .then(invoice => {
+            res.status(200).json({invoice});
         })
         .catch(err => {
             res.status(400).json(err);
