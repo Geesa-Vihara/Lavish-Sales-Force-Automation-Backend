ee699f24efd5a9a4ae51501d1670744629efdb18
Geesa-Vihara <geesavihara97@gmail.com>
2020-01-11 22:17:43 +0530

    20200111-05 top products by date

--- app/controllers/analytics.controller.js
+++ app/controllers/analytics.controller.js
@@ -43,7 +43,7 @@ exports.progress=(req,res)=>{
         "$match": {
           "year": {
             $gte: new Date(d).getFullYear()-1,
-            $lt: new Date(d).getFullYear()+1
+            $lte: new Date(d).getFullYear()
         },          
         }
       }, {$group : {
@@ -62,3 +62,28 @@ exports.progress=(req,res)=>{
       })
 }
 
+exports.topProduct=(req,res)=>{
+    var d = new Date(req.body.dateFrom);
+    var n = new Date(req.body.dateTo);
+    Invoice.aggregate([
+        
+        { $match: { orderDate :{
+            $gte: new Date(d),
+            $lt: new Date(n)
+        } } },
+        {$group : {_id : null, 
+        teapouch_sum : { $sum:{ $add: ["$teapouch20.price","$teapouch50.price","$teapouch100.price","$teapouch200.price","$teapouch400.price","$teapouch1kg1.price","$teapouch1kg2.price","$teapouch1kg3.price","$teapouch1kg4.price"]}},
+        teabag_sum : { $sum:{ $add: ["$teabag1.price","$teabag2.price","$teabag3.price"]}},
+        teasachet_sum : { $sum:{ $add: ["$teasachet1.price","$teasachet2.price","$teasachet3.price"]}},
+        teabulk_sum : { $sum:{ $add: ["$teabulk1.price","$teabulk2.price","$teabulk3.price","$teabulk4.price","$teabulk5.price","$teabulk6.price"]}},
+        teabottle_sum : { $sum:{ $add: ["$teabottle.price"]}},
+        teabasket_sum : { $sum:{ $add: ["$teabasket1.price","$teabasket2.price"]}}
+    }}])
+        .then(rep=> 
+        {
+            res.status(200).json(rep)
+        }
+        )
+        .catch(err => {res.status(400).json(err);
+        })
+}
