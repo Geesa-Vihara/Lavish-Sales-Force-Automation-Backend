217f3b7df669d6de83c1ae9358a28632141dc8c1
Geesa-Vihara <geesavihara97@gmail.com>
2020-01-11 20:54:43 +0530

    20200111-04  yearly progress

--- app/controllers/analytics.controller.js
+++ app/controllers/analytics.controller.js
@@ -29,3 +29,36 @@ exports.yearlySales=(req,res)=>{
 
       })
 }
+
+exports.progress=(req,res)=>{
+    var d = new Date();
+    Invoice.aggregate([        
+        {"$project": { 
+            "year": {
+             "$year": "$orderDate"
+          },
+          "tot":"$totalValue"
+        
+        }}, {
+        "$match": {
+          "year": {
+            $gte: new Date(d).getFullYear()-1,
+            $lt: new Date(d).getFullYear()+1
+        },          
+        }
+      }, {$group : {
+          _id :"$year",
+          sum:{$sum:"$tot"}        
+
+    }}
+    ]).then(rep=> {
+        res.status(200).json(rep)
+      })
+      .catch(err => 
+        {
+            res.status(400).json(err);
+          
+
+      })
+}
+
