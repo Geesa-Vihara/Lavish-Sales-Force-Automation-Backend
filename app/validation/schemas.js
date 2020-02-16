c041db57feaf2a22a022899f0be5cd23c22f3d05
codekcg23 <kavigamage23@gmail.com>
2020-02-17 00:07:32 +0530

    error handled

--- app/validation/schemas.js
+++ app/validation/schemas.js
@@ -1,30 +1,30 @@
 
-const Joi = require('Joi');
+// const Joi = require('Joi');
 
-const schemas = {
+// const schemas = {
 
-    salesRep : Joi.object().keys({
+//     salesRep : Joi.object().keys({
 
-        userName : Joi.string().required(),
-        fullName : Joi.string().required(),
-        nic      : Joi.string().required(),
-        area     : Joi.string().required(),
-        address  : Joi.string().required(),
-        phoneNo  : Joi.string().max(10).min(10).required(),
-        email    : Joi.string().email().lowercase().optional(),
-        password : Joi.string().required().strict(),
-        confirmPassword : Joi.string().valid(Joi.ref('password')).required().strict()
+//         userName : Joi.string().required(),
+//         fullName : Joi.string().required(),
+//         nic      : Joi.string().required(),
+//         area     : Joi.string().required(),
+//         address  : Joi.string().required(),
+//         phoneNo  : Joi.string().max(10).min(10).required(),
+//         email    : Joi.string().email().lowercase().optional(),
+//         password : Joi.string().required().strict(),
+//         confirmPassword : Joi.string().valid(Joi.ref('password')).required().strict()
     
-}),
+// }),
 
-    salesRepUpdate : Joi.object().keys({
-        userName : Joi.string().required(),
-        fullName : Joi.string().required(),
-        nic      : Joi.string().required(),
-        area     : Joi.string().required(),
-        address  : Joi.string().required(),
-        phoneNo  : Joi.string().max(10).min(10).required(),
-        email    : Joi.string().email().lowercase().optional(),
-    })
-};
-module.exports = schemas;
+//     salesRepUpdate : Joi.object().keys({
+//         userName : Joi.string().required(),
+//         fullName : Joi.string().required(),
+//         nic      : Joi.string().required(),
+//         area     : Joi.string().required(),
+//         address  : Joi.string().required(),
+//         phoneNo  : Joi.string().max(10).min(10).required(),
+//         email    : Joi.string().email().lowercase().optional(),
+//     })
+// };
+// module.exports = schemas;
